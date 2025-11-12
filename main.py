b3e82a2808a24440f4514e93160450cad4845017
RJ <rjcodes.dev@gmail.com>
2025-11-12 13:40:55 -0500

    bug: import fix, comment process

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import sys
-from PySide6.QTWidgets import QApplication
+from PySide6.QtWidgets import QApplication
 
 from src.gui.main_window import MainWindow
 
 def main():
     """Entry point for duckboard app"""
     app = QApplication(sys.argv)
+
+    #set app metadata
     app.setApplicationName("Duckboard")
     app.setOrganizationName("Duckboard")
 
+    #create main window
     window = MainWindow()
     window.show()
 
